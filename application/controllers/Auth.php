7cce6db918783cddb4e58ba15f5ec934bbcf3fc7
noto90 <mohkarnoto@gmail.com>
2017-08-25 12:25:46 +0700

    This is first commit

---
+++ application/controllers/Auth.php
@@ -0,0 +1,54 @@
+<?php
+
+Class Auth extends CI_Controller{
+    
+    
+    function __construct() {
+        parent::__construct();
+        $this->load->model('Model_login');
+        
+    }
+    
+    function index()
+    {
+        harus_logout();
+        $this->load->view('login');
+    }
+    
+    function login() {
+        //memberi validasi pada username dan password
+        harus_logout();
+        $this->form_validation->set_rules('username', '', 'required',
+               array(
+                   'required' => '<div class="alert alert-danger text-center"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                  Username tidak boleh kosong</div>'));
+        $this->form_validation->set_rules('password', '', 'required',
+               array(
+                   'required' => '<div class="alert alert-danger text-center"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                                  Password tidak boleh kosong</div>'));
+
+        $username = $this->input->post('username');
+        $pass     = $this->input->post('password');
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->load->view('login');
+        } else {
+            $cek = $this->Model_login->get_users($username, $pass);
+            if ($cek->num_rows() !== 0) {
+                $this->session->set_userdata(array('username'=>$username,'status'=>'login'));
+                redirect('Dashboard');
+            } else {
+                $this->session->set_flashdata('msg', '<div class="alert alert-danger text-center">Username atau Password Salah !!</div>');
+                $this->load->view('login');
+            }
+        }
+    }
+    
+    function logout()
+    {
+        $this->session->sess_destroy();
+        redirect('Auth');
+    }
+
+}
+
